903933a9466deb54f62e58995322814085b0b60e
HeathLinder <heathlinder@tnt.ac.nz>
2021-06-28 12:42:08 +1200

    Start_Screen Main Heading, Frame and Formatting Variables.
    I forgot to commit when I finished the Frame and the Formatting Variables so I am committing them with the Main Heading.

--- Start_Screen.py
+++ Start_Screen.py
@@ -5,7 +5,23 @@ import random
 
 class Start_Screen:
     def __init__(self, parent):
-        print("Hello World")
+        
+        # Formatting Variables
+        background_colour = "aquamarine"
+
+        # Frame
+        self.start_screen_frame = Frame(height=200, width=400, pady=10, bg=background_colour)
+        self.start_screen_frame.grid()
+
+        # Main Heading (row 0)
+        self.start_screen_label = Label(self.start_screen_frame, text="Country Quiz Game", font="Oswald 19 bold", bg=background_colour, padx=10, pady=10)
+        self.start_screen_label.grid()
+
+        # Instructions Label (row 1)
+
+        # Question Buttons (row 2, three buttons inside frame)
+
+        # Help & Game Statistics Buttons (row 3, two buttons inside frame)
 
 
 # main routine
