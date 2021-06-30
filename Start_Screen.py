64e48705009824312c71831dc47fd5bd80856f1c
HeathLinder <heathlinder@tnt.ac.nz>
2021-06-30 14:23:55 +1200

    Instructions Label
    The instructions label is complete

--- Start_Screen.py
+++ Start_Screen.py
@@ -9,17 +9,35 @@ class Start_Screen:
         # Formatting Variables
         background_colour = "aquamarine"
 
-        # Frame
+        #  Start_Screen Frame
         self.start_screen_frame = Frame(height=200, width=400, pady=10, bg=background_colour)
         self.start_screen_frame.grid()
 
         # Main Heading (row 0)
-        self.start_screen_label = Label(self.start_screen_frame, text="Country Quiz Game", font="Oswald 19 bold", bg=background_colour, padx=10, pady=10)
+        self.start_screen_label = Label(self.start_screen_frame, 
+                                        text="Country Quiz Game", font="Oswald 19 bold", 
+                                        bg=background_colour, padx=10, pady=10)
         self.start_screen_label.grid()
 
         # Instructions Label (row 1)
+        self.instructions_label = Label(self.start_screen_frame, 
+                                        text= "Please select the amount of questions " 
+                                        "you would like to do, If you get stuck or don't " 
+                                        "know how to play you can check the rules.", 
+                                        font="Oswald 10 italic", wrap=300, 
+                                        bg = background_colour, padx=10, pady=10)
+        self.instructions_label.grid(row=1)
 
-        # Question Buttons (row 2, three buttons inside frame)
+
+        # Button Frame (row 2)
+        self.question_buttons_frame = Frame()
+        self.question_buttons_frame.grid(row=2)
+
+        # 5 Question Button
+
+        # 10 Question Button
+
+        # 20 Question Button
 
         # Help & Game Statistics Buttons (row 3, two buttons inside frame)
 
