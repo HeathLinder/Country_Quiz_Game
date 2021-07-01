7e66eb8d844f48b86be86025672bb54b843ff62f
HeathLinder <heathlinder@tnt.ac.nz>
2021-07-02 10:15:45 +1200

    Help_Test button
    the help button is finished.

--- Help_screen.py
+++ Help_screen.py
@@ -11,19 +11,22 @@ class Help_Test:
 
         # Help_Test Frame
         self.help_test_frame = Frame(width=300, height=300, bg=background_colour,
-                                pady=10)
+                                pady=10, padx=10)
         self.help_test_frame.grid()
 
          # Help_Test Heading (row 0)
         self.help_test_label = Label(self.help_test_frame, text="Help Test",
-                                font=("Oswald 19 bold"), bg=background_colour, 
-                                padx=10, pady=10)
+                                    font=("Oswald 19 bold"), bg=background_colour, 
+                                    padx=10, pady=10)
         self.help_test_label.grid(row=0)
 
         # Help/Rules Button (row 1)
-        #self.help_and_rules_button = Button(self.help_test_frame, text="Help/Rules",
-                                       # font="Oswald 15", bg="yellow", padx=10, pady=10)
-       # self.help_and_rules_button.grid(row=1)
+        self.help_and_rules_button = Button(self.help_test_frame, text="Help/Rules",
+                                        font="Oswald 15", bg="yellow", padx=10, pady=10)
+        self.help_and_rules_button.grid(row=1)
+
+class Help_Screen:
+    def __init__(self, parent):
 
 
 
